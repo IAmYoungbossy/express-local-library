1008794bed62176c673025cbd0e628676f7a2a36
Letam Bossman Barinua <letambarinua@gmail.com>
2023-04-16 16:56:34 +0100

    Add book instance model in bookInstance.ts module

---
+++ src/models/bookInstance.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const BookInstanceSchema = new Schema({
+  book: {
+    type: Schema.Types.ObjectId,
+    ref: "Book",
+    required: true,
+  },
+  imprint: { type: String, required: true },
+  due_back: { type: Date, default: Date.now },
+  status: {
+    type: String,
+    required: true,
+    default: "Maintenance",
+    enum: ["Available", "Maintenance", "Loaned", "Reserved"],
+  },
+});
+
+BookInstanceSchema.virtual("url").get(function () {
+  return `/catalog/bookinstance/${this._id}`;
+});
+
+/*************************************************************
+ ** Arrow function is not used as callback in get method above
+ ** so we can always point to the right "This". **************
+ *************************************************************/
+const BookInstanceModel = mongoose.model(
+  "Author",
+  BookInstanceSchema
+);
+
+export default BookInstanceModel;
